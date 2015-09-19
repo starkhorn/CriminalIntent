14029d6117e5bd7b89169ea3e4e8a618e0271408
starkhorn <t.earendel@gmail.com>
2015-09-19 19:11:42 +0700

    Update crime on the list when its details change

--- app/src/main/java/com/starkhorn/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/starkhorn/criminalintent/CrimeListFragment.java
@@ -38,12 +38,24 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        adapter = new CrimeAdapter(crimes);
-        crimeRecyclerView.setAdapter(adapter);
+        if (adapter == null) {
+            adapter = new CrimeAdapter(crimes);
+            crimeRecyclerView.setAdapter(adapter);
+        } else {
+            adapter.notifyDataSetChanged();
+        }
+
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
