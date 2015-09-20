ba21fa4056a2dbabc912aa1a0ab9f145d85a68de
starkhorn <t.earendel@gmail.com>
2015-09-20 13:49:31 +0700

    Display crime details using ViewPager

---
+++ app/src/main/java/com/starkhorn/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,57 @@
+package com.starkhorn.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by starkhorn on 9/20/2015.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.starkhorn.criminalintent.crime_id";
+    private ViewPager viewPager;
+    private List<Crime> crimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        viewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+        crimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = crimes.get(position);
+
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return crimes.size();
+            }
+        });
+    }
+}
