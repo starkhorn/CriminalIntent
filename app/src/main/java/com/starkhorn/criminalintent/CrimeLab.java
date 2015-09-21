73b68b608d73a88bc3eb5682d089feca3bb6d893
starkhorn <t.earendel@gmail.com>
2015-09-21 23:40:10 +0700

    Handle 'New Crime' button the create a new crime

--- app/src/main/java/com/starkhorn/criminalintent/CrimeLab.java
+++ app/src/main/java/com/starkhorn/criminalintent/CrimeLab.java
@@ -24,14 +24,6 @@ public class CrimeLab {
 
     public CrimeLab(Context context) {
         crimes = new ArrayList<>();
-
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-
-            crimes.add(crime);
-        }
     }
 
     public List<Crime> getCrimes() {
@@ -46,5 +38,9 @@ public class CrimeLab {
         }
 
         return null;
+    }
+
+    public void addCrime(Crime crime) {
+        crimes.add(crime);
     }
 }
